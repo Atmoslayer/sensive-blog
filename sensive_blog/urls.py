f2d8edb294bd20885d427796d07a73ee297abd49
Atmoslayer <ddsomdim@gmail.com>
2023-01-14 16:44:06 +0300

    Little changes

--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -18,5 +18,5 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        path('__debug__/', include('debug_toolbar.urls')),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
