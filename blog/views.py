92b7cbcd4aa70559164a5efb0d01e97181fcd02f
Atmoslayer <ddsomdim@gmail.com>
2023-02-07 22:10:53 +0300

    Fix select_related

--- blog/views.py
+++ blog/views.py
@@ -55,7 +55,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    comments = post.comments.select_related()
+    comments = post.comments.select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
