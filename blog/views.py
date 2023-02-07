c830cf2140ec90b3dffb45719e5a93f08ba12b1c
Atmoslayer <ddsomdim@gmail.com>
2023-02-07 22:29:56 +0300

    Reduce not optimized functions

--- blog/views.py
+++ blog/views.py
@@ -13,15 +13,8 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all().popular()],
         'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': tag.posts.count
     }
 
 
@@ -64,7 +57,7 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.all().popular()
 
     serialized_post = {
         'title': post.title,
@@ -75,7 +68,7 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in related_tags],
+        'tags': [serialize_tag_optimized(tag) for tag in related_tags],
     }
 
     most_popular_posts = Post.objects.popular().select_related('author').prefetch_related('tags').fetch_with_comments_count()[:5]
@@ -103,7 +96,7 @@ def tag_filter(request, tag_title):
 
     context = {
         'tag': tag.title,
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
         'posts': [serialize_post_optimized(post) for post in related_posts],
         'most_popular_posts': [
             serialize_post_optimized(post) for post in most_popular_posts
