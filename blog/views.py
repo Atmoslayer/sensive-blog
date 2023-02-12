2a4ecacdbd357f5ef29712bf990d3eb5a3021174
Atmoslayer <ddsomdim@gmail.com>
2023-02-12 21:58:40 +0300

    Reduce tag queries

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 
 
 def serialize_post_optimized(post):
+    post_tags = post.tags.all().popular()
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -13,8 +14,8 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all().popular()],
-        'first_tag_title': post.tags.all()[0].title,
+        'tags': [serialize_tag_optimized(tag) for tag in post_tags],
+        'first_tag_title': post_tags[0].title,
     }
 
 
